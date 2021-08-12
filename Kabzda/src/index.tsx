d8d9ebb101e30255a87a752d01c4b92f6e27f7be
Pavel1706 <80953762+Pavel1706@users.noreply.github.com>
2021-08-12 23:21:23 +0300

    add new fails

--- Kabzda/src/index.tsx
+++ Kabzda/src/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import App from "./App";
 
 ReactDOM.render(
-  <App />,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
@@ -14,13 +15,3 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-
-
-
-
-
-
-
-
-
--- Kabzda/src/сomponents/accordion/Accordion.tsx
+++ Kabzda/src/сomponents/accordion/Accordion.tsx
@@ -1,55 +1,45 @@
 import React from "react";
-//
-//
-// type AccordionPropsType = {
-//     titleValue: string
-//     collapsed: boolean
-// }
-//
-// function Accordion(props: AccordionPropsType) {
-//         return <div>
-//             <AccordionTitle title={props.titleValue}/>
-//              {!props.collapsed &&<AccordionBody title={props.titleValue}/>}
-//         </div>
-//
-//
-// }
-//
-// type AccordionTitlePropsType = {
-//     title: string
-//
-// }
-//
-// function AccordionTitle(props: AccordionTitlePropsType) {
-//     if (props.title === 'Menu') {
-//         console.log('AccordionTitle rendering')
-//         return (
-//             <h3>Menu</h3>
-//         )
-//     }
-//     return <></>
-//
-//
-// }
-//
-// type AccordionBodyPropsType = {
-//     title: string
-//
-// }
-//
-// function AccordionBody(props: AccordionBodyPropsType) {
-//     if (props.title === 'spisok') {
-//         console.log('AccordionBody rendering')
-//         return <div>
-//             <ul>
-//                 <li>1</li>
-//                 <li>2</li>
-//                 <li>3</li>
-//             </ul>
-//         </div>
-//     }
-//     return <></>
-//
-// }
-//
-// export default Accordion;
+
+
+type AccordionPropsType = {
+    titleValue: string
+    collapsed: boolean
+}
+
+export function Accordion(props: AccordionPropsType) {
+        return <div>
+            <AccordionTitle title={props.titleValue}/>
+             {!props.collapsed &&<AccordionBody title={props.titleValue}/>}
+        </div>
+
+
+}
+
+type AccordionTitlePropsType = {
+    title: string
+
+}
+
+function AccordionTitle(props: AccordionTitlePropsType) {
+    return <h1>{props.title}</h1>
+}
+
+type AccordionBodyPropsType = {
+    title: string
+
+}
+
+function AccordionBody(props: AccordionBodyPropsType) {
+    if (props.title === 'spisok') {
+        console.log('AccordionBody rendering')
+        return <div>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </div>
+    }
+    return <></>
+
+}
